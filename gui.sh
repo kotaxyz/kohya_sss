3bebc16f63807391308b1de3487127510c6f8195
kotaxyz <105466290+kotaxyz@users.noreply.github.com>
2024-09-08 04:35:31 +0300

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Checks to see if variable is set and non-empty.
-# This is defined first, so we can use the function for some default variable values
 env_var_exists() {
   if [[ -n "${!1}" ]]; then
     return 0
@@ -19,7 +18,6 @@ if [ -d "$lib_path" ]; then
     if [ -z "${LD_LIBRARY_PATH}" ]; then
         # LD_LIBRARY_PATH is not set, set it to the lib_path
         export LD_LIBRARY_PATH="$lib_path"
-        # echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
     fi
 fi
 
@@ -32,7 +30,6 @@ fi
 # If it is run with the sudo command, get the complete LD_LIBRARY_PATH environment variable of the system and assign it to the current environment,
 # because it will be used later.
 if [ -n "$SUDO_USER" ] || [ -n "$SUDO_COMMAND" ]; then
-    echo "The sudo command resets the non-essential environment variables, we keep the LD_LIBRARY_PATH variable."
     export LD_LIBRARY_PATH=$(sudo -i printenv LD_LIBRARY_PATH)
 fi
 
@@ -50,11 +47,8 @@ fi
 
 # Check if LD_LIBRARY_PATH environment variable exists
 if [[ -z "${LD_LIBRARY_PATH}" ]]; then
-    # Set the ANSI escape sequence for yellow text
     YELLOW='\033[0;33m'
-    # Set the ANSI escape sequence to reset text color
     RESET='\033[0m'
-    
     echo -e "${YELLOW}Warning: LD_LIBRARY_PATH environment variable is not set.${RESET}"
     echo -e "${YELLOW}Certain functionalities may not work correctly.${RESET}"
     echo -e "${YELLOW}Please ensure that the required libraries are properly configured.${RESET}"
